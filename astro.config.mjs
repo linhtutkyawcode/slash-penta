aa96281176221de7a67eee5cdadd3ce8738dd09f
Lin Htut Kyaw <linhtutkyaw.code@gmail.com>
2022-12-29 04:20:47 +0630

    setup demo-ui complete

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), react()],
+  output: "server",
+  adapter: vercel()
+});
---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+const withMT = require('@material-tailwind/react/utils/withMT');
+
+module.exports = withMT({
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
